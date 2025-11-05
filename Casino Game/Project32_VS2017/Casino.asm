5a3b1279edbbd65685004cecb8aea1e864ca95ef
Orion <powerso12345@gmail.com>
2025-11-04 23:35:44 -0500

    Add game state variables for all casino games

--- Casino Game/Project32_VS2017/Casino.asm
+++ Casino Game/Project32_VS2017/Casino.asm
@@ -4,6 +4,19 @@ INCLUDE Irvine32.inc
     balance DWORD 100
     wager DWORD 20
     welcomeMsg BYTE "Welcome to the Casino!", 0dh, 0ah, 0
+    
+    ; Game variables
+    playerTotal DWORD ?
+    dealerTotal DWORD ?
+    choice DWORD ?
+    userPick DWORD ?
+    ballNumber DWORD ?
+    currentCard DWORD ?
+    betType DWORD ?
+    coinResult DWORD ?
+    reel1 DWORD ?
+    reel2 DWORD ?
+    reel3 DWORD ?
 
 .code
 main PROC
